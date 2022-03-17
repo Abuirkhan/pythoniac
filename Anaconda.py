ae86596b31c2fee6df84b5bdc16a60a364e90230
Abuirkhan <addy84@mail.ru>
2022-03-17 12:39:38 +0300

    Anaconda5

--- Anaconda.py
+++ Anaconda.py
@@ -1,9 +1,5 @@
 a = int(input())
-b = int(input())
-c = int(input())
-if (a+b+c) % 3 == 0:
-    print("Делится на три")
-elif (a+b+c) % 9 == 0:
-    print("Делится на девять")
+if a % 4 == 0  and (not a % 100 == 0) or a % 400 == 0:
+    print('YES')
 else:
-    print("Не делится")
+    print('NO')
