7ad8251e2a78ce8551ede8c555e0094bee94ef72
Abuirkhan <addy84@mail.ru>
2022-03-17 12:12:18 +0300

    Multiline

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-brand = 'Amigoscode'
-print(brand.upper())
-brand1 = 'Amigoscode'
-print(brand1.replace('A','a'))
-print(len(brand))
-print(brand == "sjhg")
-print(brand1 != 'Amigoscode')
-print('code' in brand)
-print('code' not in brand1)
+name = "Jamila"
+email = """ 
+hello {}, 
+how are you?
+It was nice talking to you
+"""
+print(email.format(name))
