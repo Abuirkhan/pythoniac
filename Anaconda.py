e070120f6dd6f634950babfa176c373808f21595
Abuirkhan <addy84@mail.ru>
2022-03-17 09:55:52 +0300

    Anaconda3

--- Anaconda.py
+++ Anaconda.py
@@ -1,8 +1,12 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if b == (a+1) and c == (b+1):
-    print('YES')
-else:
-    print('NO')
-
+d = int(input())
+if a < b and a < c and a < d:
+    print(a)
+elif b < a and b < c and b < d:
+    print(b)
+elif c < a and c < b and c < d:
+    print(c)
+elif d < a and d < b and d < c:
+    print(d)
