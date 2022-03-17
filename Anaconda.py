88db92a7b6b46872c5a2afe20fe84e345b6155ce
Abuirkhan <addy84@mail.ru>
2022-03-17 09:53:50 +0300

    Anaconda1

--- Anaconda.py
+++ Anaconda.py
@@ -1,8 +1,8 @@
-a = str(input())
-b = str(input())
-if a == b:
-    print('Пароль принят')
+a = int(input())
+b = int(input())
+c = int(input())
+if b == (a+1) and c == (b+1):
+    print('YES')
 else:
-    print('Пароль не принят')
-
+    print('NO')
 
