c9c7e0f8b61f2b0eb74c9773da7e178779ed954d
Abuirkhan <addy84@mail.ru>
2022-03-17 12:17:19 +0300

    Anaconda4

--- Anaconda.py
+++ Anaconda.py
@@ -1,12 +1,7 @@
 a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-if a < b and a < c and a < d:
-    print(a)
-elif b < a and b < c and b < d:
-    print(b)
-elif c < a and c < b and c < d:
-    print(c)
-elif d < a and d < b and d < c:
-    print(d)
+if a % 2 == 0 and a > 20:
+    print("Четное число")
+elif  a % 2 == 0 and a < 20:
+    print("Ошибка")
+else:
+    print("Нечетное чиcло")
